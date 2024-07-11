4cae42dd35f88127a817fcad4587eb4366fbaff4
scharantej <119601237+scharantej@users.noreply.github.com>
2024-07-11 23:54:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# main.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return "About Page"
+
+@app.route('/contact')
+def contact():
+    return "Contact Page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
